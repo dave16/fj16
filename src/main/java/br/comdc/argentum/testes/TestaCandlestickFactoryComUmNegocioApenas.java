0e82a44295851065fcf7527f2c0633495658ec30
dave16 <batistabtd@gmail.com>
2014-09-01 22:50:20 -0300

    Classe Alterada

--- src/main/java/br/comdc/argentum/testes/TestaCandlestickFactoryComUmNegocioApenas.java
+++ src/main/java/br/comdc/argentum/testes/TestaCandlestickFactoryComUmNegocioApenas.java
@@ -8,18 +8,15 @@ import br.com.dc.argentum.Candlestick;
 import br.com.dc.argentum.Negocio;
 import br.com.dc.argentum.reader.CandlestickFactory;
 
-public class TestaCandlestickFactory {
+public class TestaCandlestickFactoryComUmNegocioApenas {
 
-	public static void main(String [] args){
+	public static void main(String[] args) {
+		
 		Calendar hoje = Calendar.getInstance();
 		
 		Negocio negocio1 = new Negocio(40.5, 100, hoje);
-		Negocio negocio2 = new Negocio(45.0, 100, hoje);
-		Negocio negocio3 = new Negocio(39.8, 100, hoje);
-		Negocio negocio4 = new Negocio(42.3, 100, hoje);
-		Negocio negocio5 = new Negocio(20.0, 100, hoje);
 		
-		List<Negocio> negocios = Arrays.asList(negocio1,negocio2,negocio3,negocio4,negocio5);
+		List<Negocio> negocios = Arrays.asList(negocio1);
 		
 		CandlestickFactory fabrica = new CandlestickFactory();
 		Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
@@ -29,6 +26,5 @@ public class TestaCandlestickFactory {
 		System.out.println("Minimo " + candle.getMinimo());
 		System.out.println("Maximo " + candle.getMaximo());
 		System.out.println("Volume " + candle.getVolume());
-		System.out.println("Data " + candle.toString());
 	}
 }
