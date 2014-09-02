6c85188df53f337ed33f27d15edbe7440fad14bf
dave16 <batistabtd@gmail.com>
2014-09-01 22:35:41 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/dc/argentum/Negocio.java
@@ -0,0 +1,34 @@
+package br.com.dc.argentum;
+
+import java.util.Calendar;
+
+public final class Negocio {
+
+	private final double preco;
+	private final int quantidade;
+	private final Calendar data;
+	
+	
+	public Negocio(double preco, int quantidade, Calendar data) {
+		super();
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
+	
+	public double getPreco(){
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+	
+	public double getVolume(){
+		return preco * quantidade;
+	}
+}
---
+++ src/main/java/br/com/dc/argentum/reader/CandlestickFactory.java
@@ -0,0 +1,37 @@
+package br.com.dc.argentum.reader;
+
+import java.util.Calendar;
+import java.util.List;
+
+import br.com.dc.argentum.Candlestick;
+import br.com.dc.argentum.Negocio;
+
+public class CandlestickFactory {
+
+	public Candlestick constroiCandleParaData(Calendar data , List<Negocio> negocios){
+		
+		double maximo = negocios.get(0).getPreco();
+		double minimo = negocios.get(0).getPreco();
+		double volume = 0 ;
+		
+		for(Negocio negocio : negocios){
+			
+			volume += negocio.getVolume();
+			
+			if(negocio.getPreco() > maximo){
+				maximo = negocio.getPreco();
+			}else if (negocio.getPreco() < minimo){
+				minimo = negocio.getPreco();
+			}
+		}
+		
+		double abertura = negocios.get(0).getPreco();
+		double fechamento = negocios.get(negocios.size() - 1).getPreco();
+		
+		
+		return new Candlestick(abertura, fechamento, minimo, maximo, volume, data);
+		
+	}
+	
+	
+}
